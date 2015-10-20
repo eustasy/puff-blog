1744301fd536fb0912444b0927ac531bcd8d1f14
Lewis Goddard <lewisgoddard@eustasy.org>
2015-10-20 23:42:01 +0100

    Partially fix #5 "RSS feeds don't paginate."

--- _puff/cron/hourly/rss.php
+++ _puff/cron/hourly/rss.php
@@ -22,6 +22,7 @@ if ( is_writable($Sitewide['Root'].'feed.xml') ) {
 	}
 
 	krsort($Posts);
+	$Posts = array_slice($Posts, 0, 25);
 
 	$Feed = '<?xml version="1.0" encoding="UTF-8"?>
 	<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/">
