dc472dfa3b3470c779ac2d6a8f6edcb20226f696
Lewis Goddard <lewisgoddard@eustasy.org>
2016-02-14 00:48:54 +0000

    Update post-2015-10-09-hello-world.php

--- post-2015-10-09-hello-world.php
+++ post-2015-10-09-hello-world.php
@@ -1,12 +1,10 @@
 <?php
 	require_once __DIR__.'/_puff/sitewide.php';
-
 	$Page['Title'] = 'Hello World';
 	$Page['Description']  = 'Totally a blog post.';
 	$Page['Tagline']  = 'Totally a blog post.';
 	$Page['Type']  = 'Blog Post';
 	$Page['Published'] = '2015-10-09';
-	
 	require_once $Sitewide['Templates']['Header'];
 ?>
 
