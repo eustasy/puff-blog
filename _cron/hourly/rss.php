9773a28786535504b235a22564549bde2584ddce
Lewis Goddard <lewisgoddard@eustasy.org>
2017-10-16 17:35:24 +0100

    Use HTTPS when available for doctypes

--- _cron/hourly/rss.php
+++ _cron/hourly/rss.php
@@ -25,7 +25,7 @@ if ( is_writable($Sitewide['Root'].'feed.xml') ) {
 	$Posts = array_slice($Posts, 0, 25);
 
 	$Feed = '<?xml version="1.0" encoding="UTF-8"?>
-	<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/">
+	<rss version="2.0" xmlns:atom="https://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/">
 	<channel>
 		<title>'.$Sitewide['Settings']['Site Title'].'</title>
 		<link>'.$Sitewide['Settings']['Site Root'].'</link>
