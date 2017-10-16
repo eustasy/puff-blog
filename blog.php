fa1c77b97600bf30fa3db373f18a10f27a0e6885
Lewis Goddard <lewisgoddard@eustasy.org>
2017-10-16 17:38:07 +0100

    Better semantics

--- blog.php
+++ blog.php
@@ -1,10 +1,10 @@
 <?php
 	require_once __DIR__.'/_puff/sitewide.php';
 
-	$Page['Title'] = 'Blog';
-	$Page['Description']  = 'All the latest news.';
-	$Page['Tagline']  = 'All the latest news.';
-	$Page['Type']  = 'Blog Index';
+	$Page['Title'] = 'BLog';
+	$Page['Description'] = 'All the latest news.';
+	$Page['Tagline'] = 'All the latest news.';
+	$Page['Type'] = 'Blog Index';
 
 	$Blog = file_get_contents($Sitewide['Root'].'blog.json');
 	$Blog = json_decode($Blog, true);
@@ -33,23 +33,21 @@
 	$Page['Published'] = $Blog_First['Published'];
 	require_once $Sitewide['Templates']['Header'];
 
-	echo <<<H1
-<h1>Blog</h1>
-H1;
+	echo '<section>';
+	echo '<h1>Blog</h1>';
 
 	if ( !empty($Blog) ) {
 		foreach ( $Blog as $Key => $Post ) {
-			// $Post['Title']
-			// $Post['Link']
-			// $Post['Tagline']
-			// $Post['Author']
-			// $Post['Published']
-			// var_dump($Post);
+			echo '<article>';
 			echo '<h2 class="blog-post-title-link"><a href="'.$Post['Link'].'">'.$Post['Title'].'</a></h2>';
 			if ( !empty($Post['Tagline']) ) {
-				echo '<h3 class="blog-post-tagline">'.$Post['Tagline'].'</h3>';
+				echo '<p class="blog-post-tagline">'.$Post['Tagline'].'</p>';
+			}
+			if ( !empty($Post['Description']) ) {
+				echo '<p class="blog-post-description">'.$Post['Description'].'</p>';
 			}
 			echo '<p class="blog-post-attribution">Posted by '.$Post['Author'].' on '.date('l \t\h\e jS \o\f F, Y', strtotime($Post['Published'])).'.</p>';
+			echo '</article>';
 		}
 	} else {
 		echo '<h2>Sorry, there aren\'t any posts that far back.</h2>';
@@ -61,5 +59,7 @@ H1;
 	if ( $Blog_Total > $Blog_End ) {
 		echo '<a class="next" href="?page='.( $Blog_Page + 1 ).'">Next</a>';
 	}
+
+	echo '</section>';
 
 	require_once $Sitewide['Templates']['Footer'];
